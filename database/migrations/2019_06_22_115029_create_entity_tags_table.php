5b8d0dc4160d3f6a98e46fe38054f4166d1d4bda
Christian Ted Ochoa <christianted.ochoa@gmail.com>
2019-06-22 19:55:32 +0800

    Added entity tags migration

---
+++ database/migrations/2019_06_22_115029_create_entity_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntityTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entity_tags', function (Blueprint $table) {
+            $table->integer('entity_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('entity_id')->references('id')->on('entities');
+            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->primary(['entity_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entity_tags');
+    }
+}
