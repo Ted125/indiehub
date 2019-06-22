17b845399256edd4bfc2e5ee9e882b94a62ac8c7
Christian Ted Ochoa <christianted.ochoa@gmail.com>
2019-06-22 18:46:21 +0800

    Added entity comments migration

---
+++ database/migrations/2019_06_22_104225_create_entity_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntityCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entity_comments', function (Blueprint $table) {
+            $table->integer('entity_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->text('comment');
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('entity_id')->references('id')->on('entities');
+            $table->primary(['user_id', 'entity_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entity_comments');
+    }
+}
